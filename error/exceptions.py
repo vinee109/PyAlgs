c7dacd87b1e478279d79b7df2ec700dbff127bc8
Vineet Jain <jain.vinee@gmail.com>
2016-04-04 17:54:25 -0700

    initial test_list.py

--- error/exceptions.py
+++
@@ -1,8 +0,0 @@
-"""
-TODO: insert description
-"""
-__author__ = "Vineet Jain"
-
-class RemoveFromEmpty(Exception):
-    def __init__(self, message):
-        super(RemoveFromEmpty, self).__init__(message)
