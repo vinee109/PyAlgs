5fdf08a0e5493ab78cb2e851da38313d095e46ad
Vineet Jain <jain.vinee@gmail.com>
2016-04-06 11:51:15 -0700

    2 tests to make sure the string method is working correctly

--- test_list.py
+++ test_list.py
@@ -1,13 +1,29 @@
 __author__ = "Vineet Jain"
 
 import unittest
+from list import ListNode
 from list import SLinkedList
+from test_utils import expected_str
 
-class TestSinglyLinkedList(unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
+    """
+    Testing the string methods
+    """
+    def testListNodeStr(self):
+        node = ListNode("1")
+        expected = "1"
+        actual = str(node)
+        self.assertEqual(expected, actual, expected_str(expected, actual))
 
-  def test_basic(self):
-      slist = SLinkedList()
-      self.assertTrue(slist.isEmpty())
+    def testSLinkedListStr(self):
+        slist = SLinkedList()
+        slist.insertEnd(1)
+        slist.insertEnd(2)
+        slist.insertEnd(3)
+        expected = "[1, 2, 3]"
+        actual = str(slist)
+        self.assertEqual(expected, actual, expected_str(expected, actual))
+
 
 if __name__ == '__main__':
     unittest.main()
