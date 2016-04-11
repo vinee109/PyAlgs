38d93556156e14809efea750d82f6e1fb910b503
Vineet Jain <jain.vinee@gmail.com>
2016-04-10 18:57:04 -0700

    tests for singly linked list

--- test_list.py
+++ test_list.py
@@ -17,12 +17,67 @@ class TestStringMethods(unittest.TestCase):
 
     def testSLinkedListStr(self):
         slist = SLinkedList()
+        expected, actual = "[ ]", str(slist)
+        self.assertEqual(expected, actual, expected_str(expected, actual))
         slist.insertEnd(1)
         slist.insertEnd(2)
         slist.insertEnd(3)
-        expected = "[ 1 2 3 ]"
-        actual = str(slist)
+        expected, actual = "[ 1 2 3 ]", str(slist)
         self.assertEqual(expected, actual, expected_str(expected, actual))
+
+
+class TestSLinkedList(unittest.TestCase):
+    """
+    Testing insertend, removeEnd and isEmpty methods
+    """
+    def testIsEmpty(self):
+        slist = SLinkedList()
+        self.assertTrue(slist.isEmpty(), expected_str(True, False))
+        slist.insertEnd(1)
+        self.assertFalse(slist.isEmpty(), expected_str(False, True))
+        slist.insertEnd(2)
+        self.assertFalse(slist.isEmpty(), expected_str(False, True))
+        slist.removeEnd()
+        slist.removeEnd()
+        self.assertTrue(slist.isEmpty(), expected_str(True, False))
+
+    def testInsert(self):
+        slist = SLinkedList()
+        slist.insertEnd(1)
+        self.assertEqual(1, slist.size, expected_str(1, slist.size))
+        self.assertEqual("[ 1 ]", str(slist), expected_str("[ 1 ]", str(slist)))
+        slist.insertEnd(2)
+        self.assertEqual(2, slist.size, expected_str(2, slist.size))
+        self.assertEqual("[ 1 2 ]",
+                         str(slist),
+                         expected_str("[ 1 2 ]", str(slist)))
+        slist.insertEnd(3)
+        self.assertEqual(3, slist.size, expected_str(3, slist.size))
+        self.assertEqual("[ 1 2 3 ]",
+                         str(slist),
+                         expected_str("[ 1 2 3 ]", str(slist)))
+
+    def testRemove(self):
+        slist = SLinkedList()
+        slist.insertEnd(1)
+        slist.insertEnd(2)
+        slist.insertEnd(3)
+
+        self.assertEqual(3, slist.size, expected_str(3, slist.size))
+        self.assertEqual("[ 1 2 3 ]",
+                         str(slist),
+                         expected_str("[ 1 2 3 ]", str(slist)))
+        self.assertEqual(slist.removeEnd(), 3)
+        self.assertEqual(2, slist.size, expected_str(2, slist.size))
+        self.assertEqual("[ 1 2 ]",
+                         str(slist),
+                         expected_str("[ 1 2 ]", str(slist)))
+        self.assertEqual(slist.removeEnd(), 2)
+        self.assertEqual(1, slist.size, expected_str(1, slist.size))
+        self.assertEqual("[ 1 ]", str(slist), expected_str("[ 1 ]", str(slist)))
+        self.assertEqual(slist.removeEnd(), 1)
+        self.assertEqual(0, slist.size, expected_str(0, slist.size))
+        self.assertEqual("[ ]", str(slist), expected_str("[ ]", str(slist)))
 
 
 if __name__ == '__main__':
