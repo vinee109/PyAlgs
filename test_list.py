32a69752a26a3830a26b9c5938bf47d4c6bfe9f9
Vineet Jain <jain.vinee@gmail.com>
2016-04-25 10:55:39 -0700

    tests for Doubly Linked List, created LinkedListTest class to prevent code duplication because the tests are basically the same for DLists and SLists

--- test_list.py
+++ test_list.py
@@ -3,6 +3,7 @@ __author__ = "Vineet Jain"
 import unittest
 from linkedlist import ListNode
 from linkedlist import SLinkedList
+from linkedlist import DLinkedList
 from test_utils import expected_str
 
 class TestStringMethods(unittest.TestCase):
@@ -26,58 +27,84 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(expected, actual, expected_str(expected, actual))
 
 
-class TestSLinkedList(unittest.TestCase):
+class TestLinkedList(unittest.TestCase):
     """
     Testing insertend, removeEnd and isEmpty methods
     """
+    def runTestIsEmpty(self, lst):
+        self.assertTrue(lst.isEmpty(), expected_str(True, False))
+        lst.insertEnd(1)
+        self.assertFalse(lst.isEmpty(), expected_str(False, True))
+        lst.insertEnd(2)
+        self.assertFalse(lst.isEmpty(), expected_str(False, True))
+        lst.removeEnd()
+        lst.removeEnd()
+        self.assertTrue(lst.isEmpty(), expected_str(True, False))
+
+    def runTestInsertEnd(self, lst):
+        lst.insertEnd(1)
+        self.assertEqual(1, lst.size, expected_str(1, lst.size))
+        self.assertEqual("[ 1 ]", str(lst), expected_str("[ 1 ]", str(lst)))
+        lst.insertEnd(2)
+        self.assertEqual(2, lst.size, expected_str(2, lst.size))
+        self.assertEqual("[ 1 2 ]",
+                         str(lst),
+                         expected_str("[ 1 2 ]", str(lst)))
+        lst.insertEnd(3)
+        self.assertEqual(3, lst.size, expected_str(3, lst.size))
+        self.assertEqual("[ 1 2 3 ]",
+                         str(lst),
+                         expected_str("[ 1 2 3 ]", str(lst)))
+
+    def runTestRemoveEnd(self, lst):
+        lst.insertEnd(1)
+        lst.insertEnd(2)
+        lst.insertEnd(3)
+
+        self.assertEqual(3, lst.size, expected_str(3, lst.size))
+        self.assertEqual("[ 1 2 3 ]",
+                         str(lst),
+                         expected_str("[ 1 2 3 ]", str(lst)))
+        self.assertEqual(lst.removeEnd(), 3)
+        self.assertEqual(2, lst.size, expected_str(2, lst.size))
+        self.assertEqual("[ 1 2 ]",
+                         str(lst),
+                         expected_str("[ 1 2 ]", str(lst)))
+        self.assertEqual(lst.removeEnd(), 2)
+        self.assertEqual(1, lst.size, expected_str(1, lst.size))
+        self.assertEqual("[ 1 ]", str(lst), expected_str("[ 1 ]", str(lst)))
+        self.assertEqual(lst.removeEnd(), 1)
+        self.assertEqual(0, lst.size, expected_str(0, lst.size))
+        self.assertEqual("[ ]", str(lst), expected_str("[ ]", str(lst)))
+
+
+class TestSLinkedList(TestLinkedList):
+
+    def setUp(self):
+        self.lst = SLinkedList()
+
     def testIsEmpty(self):
-        slist = SLinkedList()
-        self.assertTrue(slist.isEmpty(), expected_str(True, False))
-        slist.insertEnd(1)
-        self.assertFalse(slist.isEmpty(), expected_str(False, True))
-        slist.insertEnd(2)
-        self.assertFalse(slist.isEmpty(), expected_str(False, True))
-        slist.removeEnd()
-        slist.removeEnd()
-        self.assertTrue(slist.isEmpty(), expected_str(True, False))
+        self.runTestIsEmpty(self.lst)
 
-    def testInsert(self):
-        slist = SLinkedList()
-        slist.insertEnd(1)
-        self.assertEqual(1, slist.size, expected_str(1, slist.size))
-        self.assertEqual("[ 1 ]", str(slist), expected_str("[ 1 ]", str(slist)))
-        slist.insertEnd(2)
-        self.assertEqual(2, slist.size, expected_str(2, slist.size))
-        self.assertEqual("[ 1 2 ]",
-                         str(slist),
-                         expected_str("[ 1 2 ]", str(slist)))
-        slist.insertEnd(3)
-        self.assertEqual(3, slist.size, expected_str(3, slist.size))
-        self.assertEqual("[ 1 2 3 ]",
-                         str(slist),
-                         expected_str("[ 1 2 3 ]", str(slist)))
+    def testInsertEnd(self):
+        self.runTestInsertEnd(self.lst)
 
-    def testRemove(self):
-        slist = SLinkedList()
-        slist.insertEnd(1)
-        slist.insertEnd(2)
-        slist.insertEnd(3)
+    def testRemovEnd(self):
+        self.runTestRemoveEnd(self.lst)
 
-        self.assertEqual(3, slist.size, expected_str(3, slist.size))
-        self.assertEqual("[ 1 2 3 ]",
-                         str(slist),
-                         expected_str("[ 1 2 3 ]", str(slist)))
-        self.assertEqual(slist.removeEnd(), 3)
-        self.assertEqual(2, slist.size, expected_str(2, slist.size))
-        self.assertEqual("[ 1 2 ]",
-                         str(slist),
-                         expected_str("[ 1 2 ]", str(slist)))
-        self.assertEqual(slist.removeEnd(), 2)
-        self.assertEqual(1, slist.size, expected_str(1, slist.size))
-        self.assertEqual("[ 1 ]", str(slist), expected_str("[ 1 ]", str(slist)))
-        self.assertEqual(slist.removeEnd(), 1)
-        self.assertEqual(0, slist.size, expected_str(0, slist.size))
-        self.assertEqual("[ ]", str(slist), expected_str("[ ]", str(slist)))
+class TestDLinkedList(TestLinkedList):
+
+    def setUp(self):
+        self.lst = DLinkedList()
+
+    def testIsEmpty(self):
+        self.runTestIsEmpty(self.lst)
+
+    def testInsertEnd(self):
+        self.runTestInsertEnd(self.lst)
+
+    def testRemoveEnd(self):
+        self.runTestRemoveEnd(self.lst)
 
 
 if __name__ == '__main__':
